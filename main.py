b38a7f7d07eb28fc12201baa092ec63cec933c08
idontstart <69805745+idontstart@users.noreply.github.com>
2020-10-06 10:28:51 -0400

    Update pxt.json, main.ts, main.py

--- main.py
+++ main.py
@@ -340,26 +340,26 @@ def on_jump(): #jump
 controller.A.on_event(ControllerButtonEvent.PRESSED, on_jump)
 
 def on_update(): #turn sprite
-    if controller.dx() > 0:
+    if Knight.vx > 0:
         Knight.set_image(img("""
-        . . . . . . f f f f f f . . . .
-        . . . . f f e e e e f 2 f . . .
-        . . . f f e e e e f 2 2 2 f . .
-        . . . f e e e f f e e e e f . .
-        . . . f f f f e e 2 2 2 2 e f .
-        . . . f e 2 2 2 f f f f e 2 f .
-        . . f f f f f f f e e e f f f .
-        . . f f e 4 4 e b f 4 4 e e f .
-        . . f e e 4 d 4 1 f d d e f . .
-        . . . f e e e 4 d d d d f . . .
-        . . . . f f e e 4 4 4 e f . . .
-        . . . . . 4 d d e 2 2 2 f . . .
-        . . . . . e d d e 2 2 2 f . . .
-        . . . . . f e e f 4 5 5 f . . .
-        . . . . . . f f f f f f . . . .
-        . . . . . . . f f f . . . . . .
+            . . . . . . f f f f f f . . . .
+            . . . . f f e e e e f 2 f . . .
+            . . . f f e e e e f 2 2 2 f . .
+            . . . f e e e f f e e e e f . .
+            . . . f f f f e e 2 2 2 2 e f .
+            . . . f e 2 2 2 f f f f e 2 f .
+            . . f f f f f f f e e e f f f .
+            . . f f e 4 4 e b f 4 4 e e f .
+            . . f e e 4 d 4 1 f d d e f . .
+            . . . f e e e 4 d d d d f . . .
+            . . . . f f e e 4 4 4 e f . . .
+            . . . . . 4 d d e 2 2 2 f . . .
+            . . . . . e d d e 2 2 2 f . . .
+            . . . . . f e e f 4 5 5 f . . .
+            . . . . . . f f f f f f . . . .
+            . . . . . . . f f f . . . . . .
         """))
-    if controller.dx() < 0:
+    elif Knight.vx < 0:
         Knight.set_image(img("""
             . . . . f f f f f f . . . . . .
             . . . f 2 f e e e e f f . . . .
--- main.ts
+++ main.ts
@@ -349,28 +349,26 @@ controller.A.onEvent(ControllerButtonEvent.Pressed, function on_jump() {
 })
 game.onUpdate(function on_update() {
     // turn sprite
-    if (controller.dx() > 0) {
+    if (Knight.vx > 0) {
         Knight.setImage(img`
-        . . . . . . f f f f f f . . . .
-        . . . . f f e e e e f 2 f . . .
-        . . . f f e e e e f 2 2 2 f . .
-        . . . f e e e f f e e e e f . .
-        . . . f f f f e e 2 2 2 2 e f .
-        . . . f e 2 2 2 f f f f e 2 f .
-        . . f f f f f f f e e e f f f .
-        . . f f e 4 4 e b f 4 4 e e f .
-        . . f e e 4 d 4 1 f d d e f . .
-        . . . f e e e 4 d d d d f . . .
-        . . . . f f e e 4 4 4 e f . . .
-        . . . . . 4 d d e 2 2 2 f . . .
-        . . . . . e d d e 2 2 2 f . . .
-        . . . . . f e e f 4 5 5 f . . .
-        . . . . . . f f f f f f . . . .
-        . . . . . . . f f f . . . . . .
+            . . . . . . f f f f f f . . . .
+            . . . . f f e e e e f 2 f . . .
+            . . . f f e e e e f 2 2 2 f . .
+            . . . f e e e f f e e e e f . .
+            . . . f f f f e e 2 2 2 2 e f .
+            . . . f e 2 2 2 f f f f e 2 f .
+            . . f f f f f f f e e e f f f .
+            . . f f e 4 4 e b f 4 4 e e f .
+            . . f e e 4 d 4 1 f d d e f . .
+            . . . f e e e 4 d d d d f . . .
+            . . . . f f e e 4 4 4 e f . . .
+            . . . . . 4 d d e 2 2 2 f . . .
+            . . . . . e d d e 2 2 2 f . . .
+            . . . . . f e e f 4 5 5 f . . .
+            . . . . . . f f f f f f . . . .
+            . . . . . . . f f f . . . . . .
         `)
-    }
-    
-    if (controller.dx() < 0) {
+    } else if (Knight.vx < 0) {
         Knight.setImage(img`
             . . . . f f f f f f . . . . . .
             . . . f 2 f e e e e f f . . . .
